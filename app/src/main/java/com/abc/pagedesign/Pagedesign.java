2685d869ac51d6314aaa7c180a6001c1f6b303e9
Dhairya <dhairya.khanna6668@gmail.com>
2019-01-21 23:29:02 -0800

    sign up for emails

---
+++ app/src/main/java/com/abc/pagedesign/Pagedesign.java
@@ -0,0 +1,13 @@
+package com.abc.pagedesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pagedesign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagedesign);
+    }
+}
